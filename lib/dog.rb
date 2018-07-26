15a44abb5528b0acc16780f45d57fc20420e6b03
Justine Bricknell <justinegayton@gmail.com>
2018-07-26 14:03:56 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,5 @@
 class Dog
-  attr_reader :name #dog cannot change its name, so only a reader method is needed
+  attr_reader :name, :mood #dog cannot change its name, so only a reader method is needed
   attr_accessor :mood, :owner #dog can change its mood, so it needs to be able to both get and set.
 
   def initialize(name, mood = "nervous")
